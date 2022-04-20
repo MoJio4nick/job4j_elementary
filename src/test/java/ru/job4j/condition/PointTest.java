63e359868d84cf199f6515f2fb8222b4ad9fac10
MoJio4nick <MoJio4Nick@gmail.com>
2022-04-20 23:59:04 +0300

    3. Тесты для определения расстояния между точками. [#159459 #290229]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -17,45 +17,45 @@ public class PointTest {
 
     @Test
     public void when24to46then2() {
-        double expected = 2;
-        int x1 = 2;
+        double expected = 0;
+        int x1 = 4;
         int y1 = 4;
         int x2 = 4;
-        int y2 = 6;
+        int y2 = 4;
         double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.83);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when23to41then2() {
-        double expected = 2;
-        int x1 = 2;
-        int y1 = 3;
-        int x2 = 4;
-        int y2 = 1;
+        double expected = 1;
+        int x1 = 0;
+        int y1 = 1;
+        int x2 = 0;
+        int y2 = 0;
         double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.83);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when58to96then4() {
-        double expected = 4;
-        int x1 = 5;
-        int y1 = 8;
-        int x2 = 9;
-        int y2 = 6;
+        double expected = 1;
+        int x1 = 1;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
         double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.48);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when39to87then5() {
-        double expected = 5;
-        int x1 = 3;
-        int y1 = 9;
-        int x2 = 8;
-        int y2 = 7;
+        double expected = 0;
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 1;
+        int y2 = 2;
         double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.39);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
