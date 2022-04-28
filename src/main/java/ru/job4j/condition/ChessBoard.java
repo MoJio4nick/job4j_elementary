c1767cb15f501e427fcf7cdc1b637de13abaae65
MoJio4nick <MoJio4Nick@gmail.com>
2022-04-28 15:55:34 +0300

    4.3.6. Math.abs [#373616]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,13 +3,19 @@ package ru.job4j.condition;
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
-        if (x1 < 0 || x1 > 7 || x2 < 0 || x2 > 7 || y1 < 0 || y1 > 7 || y2 < 0 || y2 > 7) {
-            return 0;
-        }
         int rsl = 0;
-        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-            rsl = Math.abs(x2 - x1);
+        if (isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2)) {
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+                rsl = Math.abs(x2 - x1);
+                rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+            }
         }
         return rsl;
     }
-} 
+
+    private static boolean isValid(int coordinate) {
+
+        return coordinate >= 0 && coordinate <= 7;
+    }
+}
