373147fd4296cdc4fb23f213eaf82b145c9640af
MoJio4nick <MoJio4Nick@gmail.com>
2022-06-18 10:33:15 +0300

    6.2. Упорядочить массив. [#4441]

---
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];   //0  1  2  3  4
+        array[0] = array[3];
+        array[3] = temp;
+        int p = array[1];
+        array[1] = array[2];
+        array[2] = p;
+        int g = array[3];
+        array[3] = array[4];
+        array[4] = g;
+        for (int h : array) {
+            System.out.println(h);
+        }
+    }
+}
