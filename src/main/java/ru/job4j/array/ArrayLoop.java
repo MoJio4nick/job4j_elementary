d24f77e82a5864cad9e44bac86e7003c64a7f0ba
MoJio4nick <MoJio4Nick@gmail.com>
2022-06-18 10:33:38 +0300

    6.2. Упорядочить массив. [#4441]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -4,7 +4,7 @@ public class ArrayLoop {
     public static void main(String[] args) {
         int[] func = new int[5];
         for (int index = 0; index < func.length; index++) {
-            func[index] =   index * 2 + 3;
+            func[index] = index * 2 + 3;
         }
             for (int funcs : func) {
                 System.out.println(funcs);
